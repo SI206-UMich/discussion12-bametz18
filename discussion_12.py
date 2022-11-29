0ef59e0e42b83a2ef7c4ae28b863d08544ca0132
bametz18 <bametz@umich.edu>
2022-11-29 17:01:10 -0500

    First Commit

--- discussion_12.py
+++ discussion_12.py
@@ -16,6 +16,8 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS Employee (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, job_id INTEGER, hire_date TEXT, salary NUMERIC)")
+    conn.commit()
     pass
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
@@ -27,10 +29,20 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
+    json_data = json.loads(file_data)
+    for dict in json_data:
+        cur.execute('INSERT OR IGNORE INTO Employee (employee_id, first_name, last_name, job_id, hire_date, salary) VALUES (?,?,?,?,?,?)', (dict["employee_id"], dict["first_name"], dict["last_name"], dict["job_id"], dict["hire_date"], dict["salary"]))
+    conn.commit()
     pass
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
+    cur.execute('SELECT Employee.hire_date, jobs.job_title FROM Employee JOIN jobs ON Employee.job_id = jobs.job_id')
+    res = cur.fetchall()
+    conn.commit()
+    print(res)
+    s = sorted(res, key=lambda x: x[0])
+    return s[0][1]
     pass
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
